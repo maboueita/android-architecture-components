fc11da6e1556205ab1a99aef3cb26b595397d8f2
maboueita <mahmoud.aboueita@gmail.com>
2017-05-19 11:37:40 +0200

    Add artifact dependencies and the main view fragment stub

---
+++ app/src/main/java/io/github/maboueita/architecturecomponentssample/BestSellersListFragment.java
@@ -0,0 +1,28 @@
+package io.github.maboueita.architecturecomponentssample;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BestSellersListFragment extends Fragment {
+
+
+    public BestSellersListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_best_sellers_list, container, false);
+    }
+
+}
