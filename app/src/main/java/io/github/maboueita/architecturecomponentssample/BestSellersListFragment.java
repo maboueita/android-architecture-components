e23ddf550bed0c8be8d963e90fbbe3595ed40851
maboueita <mahmoud.aboueita@gmail.com>
2019-05-22 11:47:22 +0200

    Update dependencies

--- app/src/main/java/io/github/maboueita/architecturecomponentssample/BestSellersListFragment.java
+++ app/src/main/java/io/github/maboueita/architecturecomponentssample/BestSellersListFragment.java
@@ -1,27 +1,21 @@
 package io.github.maboueita.architecturecomponentssample;
 
 
-import android.arch.lifecycle.LifecycleFragment;
-import android.arch.lifecycle.LiveData;
-import android.arch.lifecycle.ViewModelProvider;
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import io.github.maboueita.architecturecomponentssample.data.OverviewApiResponse;
-import io.github.maboueita.architecturecomponentssample.data.OverviewService;
-import retrofit2.Retrofit;
 
 public class BestSellersListFragment extends LifecycleFragment {
 
     private static final String TAG = BestSellersListFragment.class.getSimpleName();
 
-    OverviewApiResponse overviewApiResponse;
+    private OverviewApiResponse overviewApiResponse;
 
     public BestSellersListFragment() {
         // Required empty public constructor
@@ -38,7 +32,7 @@ public class BestSellersListFragment extends LifecycleFragment {
                 Log.d(TAG, "onActivityCreated: did data change? ");
             });
         }
-        
+
     }
 
 
